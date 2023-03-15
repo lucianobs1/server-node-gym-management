60d8e0ebc5d4a4e23ac292d9acdbe42334d7bb06
lucianobs1 <lucianotetsu@gmail.com>
2023-03-15 18:01:34 -0300

    feat: add controller and route to authenticate user

---
+++ src/http/controllers/authenticate-controller.ts
@@ -0,0 +1,35 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { AuthenticateUseCase } from '@/use-cases/authenticate-use-case';
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import z from 'zod';
+
+export async function authenticateController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const registerUseCase = new AuthenticateUseCase(usersRepository);
+
+    await registerUseCase.execute({
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message });
+    }
+
+    throw error;
+  }
+
+  return reply.status(200).send();
+}

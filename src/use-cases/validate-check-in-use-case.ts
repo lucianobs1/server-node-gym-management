43d23a97697e0cb938cc3e9899b27b6bde5eac50
lucianobs1 <lucianotetsu@gmail.com>
2023-03-20 23:13:44 -0300

    chore: add check-in validation to validate check-in in max 20 minutes

--- src/use-cases/validate-check-in-use-case.ts
+++ src/use-cases/validate-check-in-use-case.ts
@@ -1,6 +1,8 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 import { CheckIn } from '@prisma/client';
+import dayjs from 'dayjs';
+import { LateValidationCheckInError } from './errors/late-check-in-validation-error';
 import { MaxDistanceError } from './errors/max-distance-error';
 import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
@@ -23,6 +25,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateValidationCheckInError();
     }
 
     checkIn.validated_at = new Date();

603526d9e85553c0e1a662db5cf7d577e90a4d1d
lucianobs1 <lucianotetsu@gmail.com>
2023-03-21 20:54:36 -0300

    chore:implements method findManyByUserId with pagaination at check-ins-repository

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -32,8 +32,17 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
     return checkIn;
   }
   async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
-    throw new Error('Method not implemented.');
+    const checkIns = await prisma.checkIn.findMany({
+      where: {
+        user_id: userId,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return checkIns;
   }
+
   async findByUserIdOnDate(
     userId: string,
     date: Date

0e165e23fdc0a79e8f2b5c5a587c2ce06f47b7b9
lucianobs1 <lucianotetsu@gmail.com>
2023-03-30 21:07:18 -0300

    feat: test register e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-import { randomUUID } from 'crypto';
+import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
 import { Environment } from 'vitest';
 import { PrismaClient } from '@prisma/client';
@@ -9,7 +9,7 @@ const prisma = new PrismaClient();
 
 function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable');
+    throw new Error('Please provide a DATABASE_URL environment variable.');
   }
 
   const url = new URL(process.env.DATABASE_URL);
@@ -34,6 +34,7 @@ export default <Environment>{
         await prisma.$executeRawUnsafe(
           `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
         );
+
         await prisma.$disconnect();
       },
     };

051da0178631aedea2df99a2974e0ce01d4c48ab
lucianobs1 <lucianotetsu@gmail.com>
2023-04-17 18:17:24 -0300

    feat: create e2e test to search gym

--- src/http/controllers/gyms/search-controller.ts
+++ src/http/controllers/gyms/search-controller.ts
@@ -4,20 +4,20 @@ import { z } from 'zod';
 
 export async function search(request: FastifyRequest, reply: FastifyReply) {
   const searchGymsQuerySchema = z.object({
-    query: z.string(),
+    q: z.string(),
     page: z.coerce.number().min(1).default(1),
   });
 
-  const { query, page } = searchGymsQuerySchema.parse(request.query);
+  const { q, page } = searchGymsQuerySchema.parse(request.query);
 
   const searchGymsUseCase = makeSearchGymsUseCase();
 
   const { gyms } = await searchGymsUseCase.execute({
-    query,
+    query: q,
     page,
   });
 
-  return reply.status(201).send({
+  return reply.status(200).send({
     gyms,
   });
 }
